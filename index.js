8dbd952ecd849bf3beceeb752b1ee2291032b5a7
ohdudeeee <60343294+ohdudeeee@users.noreply.github.com>
2020-02-22 11:36:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,21 @@ bot.on('message', async message => {
     'bede grał tak jak mi sie podoba, chce grac czarnucha  co wyznaje kulture hiphopową z lat 90? to bede grał. CHce grać graficiarza? Będę grał. Nawet kurwa se bede grał czarnucha co popierdala z kataną na plecach i tez chuj ci do tego'
     ]
     
+    const wrzuty = [
+    'https://i.imgur.com/sJLpezz.png',
+    'https://i.imgur.com/QQTW7JA.png',
+    'https://i.imgur.com/4BmJhvk.png',
+    'https://i.imgur.com/yUROlPf.png',
+    'https://i.imgur.com/EMgOUdf.png',
+    'https://i.imgur.com/5d0k7Zk.png'
+    ]
+    
     if(cmd === `${prefix}oldschool`) {
         return message.channel.send(teksty[Math.floor(Math.random() * teksty.length)]);
+    }
+    
+    if(cmd === `${prefix}bomb`) {
+        return message.channel.send(`Yaa, Oldschool`, {file: wrzuty[Math.floor(Math.random() * wrzuty.length)]});
     }
     
     if(cmd === `${prefix}kamil`) {
